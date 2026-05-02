09ba542a9319a6f0ecf004b0421cc61bc4ae3d6f
seeobcholakyan3-source <seeobcholakyan3@gmail.com>
2026-05-01 22:21:36 -0700

    Update ARCoreScanner.kt

--- Android-App/app/src/main/kotlin/com/omniscan/xr/ARCoreScanner.kt
+++ Android-App/app/src/main/kotlin/com/omniscan/xr/ARCoreScanner.kt
@@ -1,7 +1,7 @@
 
 /* ============================================================================
  * PROPRIETARY AND CONFIDENTIAL
- * OmniScan-XR System - Copyright (c) 2026 Serob Cholakyan
+ * OmniScan-XR2 System - Copyright (c) 2026 Serob Cholakyan
  * This code is protected under the OmniScan-XR Proprietary License.
  * Commercial use or unauthorized field mining operations are strictly prohibited.
  * ============================================================================ */
