fb2e4abb378e27f44d1be00eb8cbe9e7c0ea67ea
seeobcholakyan3-source <seeobcholakyan3@gmail.com>
2026-05-02 23:57:28 -0700

    Create logging.py

---
+++ core/logging.py
@@ -0,0 +1,8 @@
+import logging
+from config import Config
+
+def setup_logging():
+    logging.basicConfig(
+        level=Config.LOG_LEVEL,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
