f71e2cf944f2f667fc84c43844cde076a5d06381
seeobcholakyan3-source <seeobcholakyan3@gmail.com>
2026-05-03 20:20:32 -0700

    Refactor config.py to remove dotenv and add new vars
    Removed dotenv loading and updated configuration variables.

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    PORT = int(os.getenv("PORT", 5001))
     NASA_API_KEY = os.getenv("NASA_API_KEY")
-    NASA_API_URL = os.getenv("NASA_API_URL", "")
-    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    NASA_API_BASE = os.getenv("NASA_API_BASE", "https://api.nasa.gov")
+
+    EARTHDATA_USERNAME = os.getenv("EARTHDATA_USERNAME")
+    EARTHDATA_PASSWORD = os.getenv("EARTHDATA_PASSWORD")
